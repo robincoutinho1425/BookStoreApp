8e49afddc0171207e03249da5f56fa5723020fb9
Robin Coutinho <coutinhorobin1425@gmail.com>
2025-01-24 00:07:08 +0530

    Update Course.jsx

--- Frontend/src/components/Course.jsx
+++ Frontend/src/components/Course.jsx
@@ -8,7 +8,7 @@ function Course() {
   useEffect(() => {
     const getBook = async () => {
       try {
-        const res = await axios.get("http://localhost:4001/book");
+        const res = await axios.get("https://bookstoreapp-backend-fhug.onrender.com/book");
         console.log(res.data);
         setBook(res.data.filter((data) => data.category !== "free"));
       } catch (error) {
